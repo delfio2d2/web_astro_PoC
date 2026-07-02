cb9565e7dd679fd2174bbc87a794df8d8f9c6722
MitoFears <delfio2d2@hotmail.com>
2026-07-02 09:12:58 +0200

    vamos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless'; // Importación limpia del adaptador
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'hybrid', // Modo Híbrido: Todo estático por defecto, SSR preparado
-  adapter: vercel(), // Pasamos el adaptador limpio de parámetros para heredar los valores estables por defecto
   devToolbar: {
-    enabled: false // Mantenemos el lienzo de desarrollo libre de barras flotantes
+    enabled: false // Mantenemos el lienzo de desarrollo limpio
   }
 });
