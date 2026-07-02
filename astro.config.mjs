0bb8d8eb0bc837b9c21c3ba2d4772950db8c341c
MitoFears <delfio2d2@hotmail.com>
2026-07-02 08:45:52 +0200

    commit para vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless'; // Importación manual del adaptador serverless
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server' // <-- Cambiado de 'serverless' a 'server'
+  output: 'hybrid', // Arquitectura Híbrida: Todo estático (SSG) por defecto, SSR preparado
+  adapter: vercel(), // Vinculación automática con la infraestructura serverless de Vercel
+  devToolbar: {
+    enabled: false // Desactivamos la barra de herramientas para limpiar el lienzo de desarrollo
+  }
 });
